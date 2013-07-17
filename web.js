5ccbbe20cb18eb2f8c64236adaa94bbf49325248
Anup Arakkal <anup.anup2001@gmail.com>
2013-07-18 00:20:18 +0530

    Started homework 4

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
     buf = fs.readFileSync('index.html');
     response.send(buf.toString());
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
